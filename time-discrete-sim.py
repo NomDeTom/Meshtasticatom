17eac58f4b4ad97e9dd2fecbfb016e9cfa348b7d
zandi <the.zandi@gmail.com>
2026-04-27 16:08:00 -0400

    Clean up 'times'/'n_times' argument, make sure we have minimum number of test runs
    We (hardcoded) throw away the 5 slowest test runs, so must have 6 or more
    to not crash. Just gate this argument with a manual check/exception.

--- time-discrete-sim.py
+++ time-discrete-sim.py
@@ -11,7 +11,7 @@ from lib.node import NodeConfig, default_generate_node_list
 
 conf = CONFIG
 
-def run_discrete_sim_timeit(nr_nodes: int, n_times: int, enable_connectivity_map: bool):
+def run_discrete_sim_timeit(nr_nodes: int, times: int, enable_connectivity_map: bool):
     def init_and_run_discrete_sim():
         random.seed(conf.SEED) # arbitrary seed rng for consistency between runs
         conf.ENABLE_CONNECTIVITY_MAP = enable_connectivity_map
@@ -30,7 +30,7 @@ def run_discrete_sim_timeit(nr_nodes: int, n_times: int, enable_connectivity_map
 
     # number set to 1, since this is heavy weight enough that a single run will take
     # an easily visible time
-    results = timeit.repeat('init_and_run_discrete_sim()', repeat=parsed_arguments.n_times, number=1, globals=locals())
+    results = timeit.repeat('init_and_run_discrete_sim()', repeat=times, number=1, globals=locals())
 
     # take average ignoring 5 slowest runs, assuming some other very unlucky things were happening
     # on the system.
@@ -49,16 +49,20 @@ parser = argparse.ArgumentParser(
     description='basic benchmark of discrete simulation. Uses timeit, which disables garbage collection by default.'
     )
 parser.add_argument('nr_nodes', type=int, help='number of nodes to generate')
-parser.add_argument('n_times', nargs='?', type=int, default=25, help='number of times to run a discrete sim to gather timing data')
-parser.add_argument('--enable-connectivity-map', action='store_true', help='enable the connectivity map optimization. Default false')
+parser.add_argument('--times', type=int, default=25, help='number of times to run a discrete sim to gather timing data. Default: 25')
+parser.add_argument('--enable-connectivity-map', action='store_true', help='enable the connectivity map optimization. Default: False')
 
 parsed_arguments = parser.parse_args()
 
-# do timing run using timeit. More involved profiling should use cProfile
-print(f"running default configuration discrete sim of {parsed_arguments.nr_nodes} nodes, {parsed_arguments.n_times} times. connectivity map enabled: {parsed_arguments.enable_connectivity_map}")
+if parsed_arguments.times <= 5:
+    # hardcoded remove of 5 slowest runs -> must have 6 or more runs
+    raise ValueError(f"Times must be 6 or more. You gave: {parsed_arguments.times}")
 
-results = run_discrete_sim_timeit(parsed_arguments.nr_nodes, parsed_arguments.n_times, parsed_arguments.enable_connectivity_map)
+# do timing run using timeit. More involved profiling should use cProfile
+print(f"running default configuration discrete sim of {parsed_arguments.nr_nodes} nodes, {parsed_arguments.times} times. connectivity map enabled: {parsed_arguments.enable_connectivity_map}")
+
+results = run_discrete_sim_timeit(parsed_arguments.nr_nodes, parsed_arguments.times, parsed_arguments.enable_connectivity_map)
 
 print(f"fastest time (in seconds): {results['fastest']}")
-print(f"average time (in seconds), ignoring slowest outliers: {results['modified_average']}")
+print(f"average time (in seconds), ignoring 5 slowest outliers: {results['modified_average']}")
 print(f"{results['all_results']=}")
